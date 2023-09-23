a50d700fb1deb4dd243ac474bdae5bad607a9607
Admin <Admin@DESKTOP-34CAHMM>
2023-09-24 01:36:38 +0400

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,9 @@
+﻿// Space Complexity: O(n)
+// Time Complexity: O(n^2)
+void BubbleSort<T>(T[] arr) where T : IComparable
+{
+    for (int i = 0; i < arr.Length; i++)
+        for (int j = 0; j < arr.Length - i - 1; j++)
+            if (arr[j].CompareTo(arr[j + 1]) > 0)
+                (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]); // tuple deconstruction
+}
